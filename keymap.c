14e77af9dbf3aa6346b44916e78ee45d471a7c50
racagogi <racagogi@gmail.com>
2025-03-05 13:12:12 +0900

    fix macro

--- keymap.c
+++ keymap.c
@@ -152,26 +152,32 @@ process_record_user(uint16_t keycode, keyrecord_t* record)
     case TRICOLN:
       if (record->event.pressed) {
         SEND_STRING(":::");
+        break;
       }
     case TRIDOT:
       if (record->event.pressed) {
         SEND_STRING("...");
+        break;
       }
     case EXACEQ:
       if (record->event.pressed) {
         SEND_STRING("===");
+        break;
       }
     case EXACNEQ:
       if (record->event.pressed) {
         SEND_STRING("!==");
+        break;
       }
     case BIND:
       if (record->event.pressed) {
         SEND_STRING(">>=");
+        break;
       }
     case GE:
       if (record->event.pressed) {
         SEND_STRING("<=");
+        break;
       }
     case VI_VSP:
       if (record->event.pressed) {
@@ -296,13 +302,13 @@ combo_t key_combos[] = {
 #define LMOD KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO
 #define TRROW KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 
-/* #define THUM(x0, x1, x2, x3) x0, x1, x2, x3
+#define THUM(x0, x1, x2, x3) x0, x1, x2, x3
 #define KCS(x0, x1, x2, x3, x4, x5, x6)                                        \
-  LAYOUT_split_3x5_2(x0, x1, x2, x3, x4, x5, x6) */
-#define KCS(x0, x1, x2, x3, x4, x5, x6)                                        \
+  LAYOUT_split_3x5_2(x0, x1, x2, x3, x4, x5, x6)
+/* #define KCS(x0, x1, x2, x3, x4, x5, x6)                                        \
                                                                                \
   LAYOUT_split_3x5_3(x0, x1, x2, x3, x4, x5, x6)
-#define THUM(x0, x1, x2, x3) KC_NO, x0, x1, x2, x3, KC_NO
+#define THUM(x0, x1, x2, x3) KC_NO, x0, x1, x2, x3, KC_NO */
 /* #define KCS(x0, x1, x2, x3, x4, x5, x6) \
   LAYOUT_planck_grid(x0, \
                      KC_NO, \
@@ -346,14 +352,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                ROW(KC_NO, KC_RPRN, KC_RCBR, KC_RBRC, KC_TILD),
                ROHLW(KC_9, KC_7, KC_5, KC_3, KC_NO),
                ROHRW(KC_NO, KC_2, KC_4, KC_6, KC_8),
-               ROW(KC_PLUS, KC_AMPR, KC_F11, KC_1, KC_NO),
+               ROW(KC_PLUS, KC_ASTR, KC_F11, KC_1, KC_NO),
                ROW(KC_NO, KC_0, KC_F12, KC_PIPE, KC_AMPR),
                THUM(MO(_Mouse), KC_SPC, KC_BSPC, MO(_Mouse))),
   [_Mov] = KCS(ROW(KC_CIRC, KC_LBRC, KC_LCBR, KC_LPRN, KC_NO),
                ROW(KC_NO, KC_RPRN, KC_RCBR, KC_RBRC, KC_TILD),
                ROW(KC_HOME, KC_PGUP, KC_PGDN, KC_END, KC_NO),
                ROW(KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT),
-               ROW(KC_PLUS, KC_AMPR, KC_NO, KC_NO, KC_NO),
+               ROW(KC_PLUS, KC_ASTR, KC_NO, KC_NO, KC_NO),
                ROW(KC_NO, KC_NO, KC_NO, KC_PIPE, KC_AMPR),
                THUM(MO(_Mouse), KC_SPC, KC_BSPC, MO(_Mouse))),
   [_Puc] = KCS(ROW(LE, GE, LARROW, ARROW, KC_NO),
